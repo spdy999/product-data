48163a02c785752900d260bb810e8b940778f394
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-05-14 07:42:58 +0700

    update search suggestion

--- lib/datasearch.dart
+++ lib/datasearch.dart
@@ -48,16 +48,29 @@ class DataSearch extends SearchDelegate<String> {
         if (!snapshot.hasData) {
           return Center(child: CircularProgressIndicator());
         }
-        final numberList =
-            snapshot.data.map((article) => article.number).toList();
-        print(numberList[0]);
+        var numberList = [];
+        for (var i = 0; i < snapshot.data.length; i++) {
+          numberList.add(snapshot.data[i].number);
+        }
 
-        final suggestionList = query.isEmpty ? numberList : numberList;
+        final suggestionList = query.isEmpty
+            ? ['8850348204071', '8851932369001', '8851932369411']
+            : numberList.where((p) => p.startsWith(query)).toList();
 
-        print(suggestionList);
         return ListView.builder(
           itemBuilder: (context, index) => ListTile(
-                title: Text(suggestionList[index]),
+                title: RichText(
+                  text: TextSpan(
+                      text: suggestionList[index].substring(0, query.length),
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                            text: suggestionList[index].substring(query.length),
+                            style: TextStyle(color: Colors.grey))
+                      ]),
+                ),
+                // title: Text(suggestionList[index]),
               ),
           itemCount: suggestionList.length,
         );
