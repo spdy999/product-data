7b136ad715eb44abe6380bca237bac30bfe5f706
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-05-14 08:38:33 +0700

    update search view

--- lib/datasearch.dart
+++ lib/datasearch.dart
@@ -57,33 +57,137 @@ class DataSearch extends SearchDelegate<String> {
           numberList.add(number);
           newObject[number] = snapshot.data[i];
         }
-        print('newObject//////////////////////////////////////////');
-        print(newObject);
 
         final suggestionList = query.isEmpty
             ? ['8850348204071', '8851932369001', '8851932369411']
             : numberList.where((p) => p.startsWith(query)).toList();
 
+        // return ListView.separated(
+        //   itemBuilder: (context, index) {
+        //     return InkWell(
+        //       child: Stack(
+        //         children: <Widget>[
+        //           Container(
+        //             padding:
+        //                 EdgeInsets.only(left: 16.0, right: 16.0, top: 24.0),
+        //             child: Column(
+        //               children: [
+        //                 ListTile(
+        //                   title: Text(
+        //                     suggestionList[index],
+        //                     style: TextStyle(fontSize: 20.0),
+        //                   ),
+        //                   subtitle: Text(
+        //                       newObject[suggestionList[index]].description),
+        //                 )
+        //               ],
+        //             ),
+        //           )
+        //         ],
+        //       ),
+        //     );
+        //   },
+        //   separatorBuilder: (context, index) => Divider(
+        //         height: 1.0,
+        //       ),
+        //   itemCount: suggestionList.length,
+        // );
+
         return ListView.builder(
-          itemBuilder: (context, index) => ListTile(
-                onTap: () {
-                  showResults(context);
-                },
-                title: RichText(
-                  text: TextSpan(
-                      text: suggestionList[index].substring(0, query.length),
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                      children: [
-                        TextSpan(
-                            text: suggestionList[index].substring(query.length),
-                            style: TextStyle(color: Colors.grey))
-                      ]),
+            itemCount: suggestionList.length,
+            padding: const EdgeInsets.all(10.0),
+            itemBuilder: (context, index) {
+              if (index.isOdd) return Divider();
+              return Column(children: [
+                Text(
+                  suggestionList[index],
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.deepOrangeAccent,
+                  ),
                 ),
-                // title: Text(suggestionList[index]),
-              ),
-          itemCount: suggestionList.length,
-        );
+                Text(
+                  '${newObject[suggestionList[index]].description}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.deepOrangeAccent,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Unit Packs',
+                        textScaleFactor: 1.0,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Pack Price',
+                        textScaleFactor: 1.0,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Value Included Vat',
+                        textScaleFactor: 1.0,
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        '${newObject[suggestionList[index]].unitPacks}',
+                        textScaleFactor: 1.8,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        '${newObject[suggestionList[index]].packPrice}',
+                        textScaleFactor: 3.0,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        '${newObject[suggestionList[index]].valueIncludedVat}',
+                        textScaleFactor: 1.8,
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                )
+              ], crossAxisAlignment: CrossAxisAlignment.start);
+            });
+
+        // return ListView.builder(
+        //   itemBuilder: (context, index) => ListTile(
+        //         onTap: () {
+        //           showResults(context);
+        //         },
+        //         title: RichText(
+        //           text: TextSpan(
+        //               text: suggestionList[index].substring(0, query.length),
+        //               style: TextStyle(
+        //                   color: Colors.black, fontWeight: FontWeight.bold),
+        //               children: [
+        //                 TextSpan(
+        //                     text: suggestionList[index].substring(query.length),
+        //                     style: TextStyle(color: Colors.grey))
+        //               ]),
+        //         ),
+        //         // title: Text(suggestionList[index]),
+        //       ),
+        //   itemCount: suggestionList.length,
+        // );
       },
     );
   }
